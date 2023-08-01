7a9f4218098f5242c91a868eda0e92ffbff21aa5
Hubert <hubertkawalko@gmail.com>
2023-08-01 20:00:42 +0200

    added remove comment option

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -4,19 +4,54 @@ import React, {useState} from "react";
 import Comment from "./comment";
 import {TextField} from "@mui/material";
 import Button from "@mui/material/Button";
-import {sendComment} from "../../services/comment-services";
+import {sendComment, deleteComment} from "../../services/comment-services";
 import {useAuth} from "../../hooks/useAuth";
+import Player from "../player/player";
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+
+const useStyles = makeStyles({
+    comments: {
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'relative',
+        alignItems: 'center',
+        left: '5%',
+        marginBottom: '16px',
+    },
+    fillerIcon: {
+    width: 32, // Adjust the size to match your other icons
+    height: 24,
+    marginRight: 8, // Adjust spacing as needed
+    },
+})
 
 export default function Comments({comments, objectId, contentType}) {
 
     const [newComment, setNewComment] = useState('');
     const {authData} = useAuth()
+    const classes = useStyles();
+    const [actualComments, setComments] = useState(comments);
 
-    const handleSetComment = () => {
-        sendComment(authData.user.id, newComment, contentType, objectId, authData.token)
-            .then( resp => {
+    const handleSetComment = async () => {
+        const comment = await sendComment(authData.user.id, newComment, contentType, objectId, authData.token);
+        if (comment){
+                console.log("dobrze")
                 setNewComment('');
-            })
+                actualComments.push(comment.result);
+            }
+    }
+
+    const handleDeleteComment = async (id) => {
+        const deleted = await deleteComment(id, authData.token);
+        if(deleted){
+            setComments((prevComments) => prevComments.filter((comment) => comment.id !== id));
+        }
+    }
+
+    const isCommentOwner = (ownerId) => {
+        return authData.user.id === ownerId
     }
 
     return (
@@ -28,10 +63,18 @@ export default function Comments({comments, objectId, contentType}) {
             </Button>
 
             <h2>Komentarze:</h2>
-            {comments.map ( comment => {
-            return (
-                <Comment comment={comment} key={comment.id} />
-                )
+            {actualComments.map ( comment => {
+            return <div className={classes.comments} key={comment.id}>
+                { isCommentOwner(comment.user.id) ?
+                    <IconButton aria-label="delete comment" onClick={() => handleDeleteComment(comment.id)}>
+                        <RemoveCircleIcon/>
+                    </IconButton>
+                    :(
+                    <div className={classes.fillerIcon}/>
+          )}
+                <Player player={comment.user.player} />
+                <p>{comment.description}</p>
+            </div>
             })}
         </div>
     )
--- src/services/comment-services.js
+++ src/services/comment-services.js
@@ -10,6 +10,18 @@ export function sendComment(user, description, contentType, ObjectId, token) {
         body: JSON.stringify({user, description, content_type: contentType, object_id: ObjectId})
     }).then(status).catch(e => {
         console.log(e)
-        console.log(JSON.stringify({user, description, content_type: contentType, object_id: ObjectId}))
+    })
+}
+
+export function deleteComment(id, token) {
+    return fetch(`http://127.0.0.1:8000/api/comments/delete_comment`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`
+        },
+        body: JSON.stringify(id)
+    }).then(status).catch(e => {
+        console.log(e)
     })
 }
