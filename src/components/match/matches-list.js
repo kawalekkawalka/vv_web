d77b648223a58016bf933b32c0855ded39f254b0
Hubert <hubertkawalko@gmail.com>
2023-11-08 23:36:58 +0100

    Revert "matchperformance model, serializer and endpoint with proper params"
    This reverts commit 514fb2252ba2e5e18d5d8f4e48e8fb8576750825.

--- src/components/match/matches-list.js
+++ src/components/match/matches-list.js
@@ -16,42 +16,12 @@ export default function MatchesList({params}) {
     const [matches, loading, error] = useFetchMatches(params)
     const format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-    function calculateSetResults(match) {
-        const setResults = [];
-        let team1 = 0;
-        let team2 = 0;
-
-        for (let i = 1; i <= 5; i++) {
-            const team1Score = match[`set${i}_team1_score`];
-            const team2Score = match[`set${i}_team2_score`];
-
-            if(team1Score == 0 && team2Score == 0) {
-                break;
-            }
-            if (team1Score > team2Score) {
-                team1++;
-            } else if (team1Score < team2Score) {
-                team2++;
-            }
-        }
-        if (team1 != 0 || team2 != 0) {
-            setResults.push({team1score: team1, team2score: team2});
-            return setResults
-        }
-        return null;
-    }
-
     return (
         <div>
             {matches && matches.map(match => {
-                const matchTime = DateTime.fromFormat(match.time, format);
-                const matchScore = calculateSetResults(match);
+                const matchTime = DateTime.fromFormat(match.time, format)
                 return <div key={match.id}>
-                    <h2 >{match.team1_name} vs {match.team2_name} {matchScore &&
-                        <div>
-                            ({matchScore[0].team1score}:{matchScore[0].team2score})
-                        </div>
-                    }</h2>
+                    <h2 >{match.team1_name} vs {match.team2_name} </h2>
                     <h3>{matchTime.toFormat('yyyy-MM-dd HH:mm')}</h3>
                 </div>
             })}
