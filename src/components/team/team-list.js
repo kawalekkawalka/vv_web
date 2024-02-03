f4b640ea670a114b350a4b91e14987873f31c50f
Hubert <hubertkawalko@gmail.com>
2024-02-03 14:01:16 +0100

    added date display in player teams view

--- src/components/team/team-list.js
+++ src/components/team/team-list.js
@@ -1,28 +1,54 @@
-import React, {useState, useEffect} from "react";
-import {useFetchTeams} from "../../hooks/fetch-teams";
+import React from "react";
+import { useFetchTeams } from "../../hooks/fetch-teams";
 import Paper from "@mui/material/Paper";
-import {lighten} from "polished";
+import { lighten } from "polished";
 import Team from "../team/team";
 
-function TeamList({params}) {
-    const [teams, loading, error] = useFetchTeams(params);
-    const backgroundColor = lighten(0.05, '#282c34');
+function TeamList({ params }) {
+  const [teams, loading, error] = useFetchTeams(params);
+  const backgroundColor = lighten(0.05, "#282c34");
+
   return (
     <div>
-        {teams && teams.map(team => {
-            return <Paper
-                          elevation={3}
-                          sx={{
-                            padding: '20px',
-                            margin: '20px',
-                            backgroundColor: backgroundColor,
-                            color: 'white',
-                            paddingTop: '5px'
-                          }}
-                        >
-                            <Team team={team}></Team>
-                        </Paper>
-        })}
+      {teams &&
+        teams.map((team) => (
+          <Paper
+            key={team.id}
+            elevation={3}
+            sx={{
+              padding: '20px',
+              margin: '20px',
+              backgroundColor: backgroundColor,
+              color: 'white',
+              paddingTop: '5px'
+            }}
+          >
+            <Team team={team} />
+            {team.date_joined && team.date_left && (
+              <div>
+                Daty występów:{' '}
+                {new Date(team.date_joined).toLocaleString('pl-PL', {
+                  month: 'long',
+                  year: 'numeric',
+                })}{' '}
+                -{' '}
+                {new Date(team.date_left).toLocaleString('pl-PL', {
+                  month: 'long',
+                  year: 'numeric',
+                })}
+              </div>
+            )}
+            {team.date_joined && !team.date_left && (
+              <div>
+                Data dołączenia:{' '}
+                {new Date(team.date_joined).toLocaleString('pl-PL', {
+                  month: 'long',
+                  year: 'numeric',
+                })}
+              </div>
+            )}
+          </Paper>
+        ))}
     </div>
   );
 }
